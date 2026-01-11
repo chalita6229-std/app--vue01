a2c0eba31e97ef316a494bcf794d5befd65a6618
ชืchalita6229-std <chalitabuaphan23@gmail.com>
2026-01-11 16:48:53 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,7 @@
+<template >
+    <div>
+        <h1>contact to Chalita Wongsurin</h1>
+        <h3>Tel.063-803-7275</h3>
+        <img alt="Vue logo" src="../assets/aom.jpg">
+    </div>
+</template>
